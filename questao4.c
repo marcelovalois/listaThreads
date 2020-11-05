d7dbfbcc646b5da99e855a5a8dc7d26b417926c5
Marcelo <mmv147.valois@gmail.com>
2020-11-05 00:48:31 -0300

    Primeira versão

---
+++ questao4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <pthread.h> 
+#include <stdlib.h>
+
+#define BUFFER_SIZE 10
+#define N 2
+
+typedef struct pessoa {
+    char genero;
+    int idade;
+} pessoa;
+
+int buff[BUFFER_SIZE];
+
+void funexec(pessoa a) {
+    printf("%c %d\n", a.genero, a.idade);
+}
+
+void agendarExecucao(void (*fun)(pessoa), pessoa p) {
+    (*fun)(p);
+}
+
+
+
+
+int main() {
+
+    pessoa x;
+    x.genero = 'm'; x.idade = 23;
+
+    agendarExecucao(funexec, x);
+
+
+
+
+
+
+   return 0;
+}
