46e959db4c0aafa347ee8488733fc4b66befe71c
Marcelo <mmv147.valois@gmail.com>
2020-11-07 01:28:51 -0300

    segunda tentativa

--- questao4.c
+++ questao4.c
@@ -3,34 +3,53 @@
 #include <stdlib.h>
 
 #define BUFFER_SIZE 10
-#define N 2
+#define N 5
 
-typedef struct pessoa {
-    char genero;
-    int idade;
-} pessoa;
+typedef struct fatores {
+    int a;
+    int b;
+} fatores;
 
-int buff[BUFFER_SIZE];
+typedef struct buffer {
+    void* funcao;
+    fatores parametro;
+} buffer;
 
-void funexec(pessoa a) {
-    printf("%c %d\n", a.genero, a.idade);
+buffer buff[BUFFER_SIZE];
+int available = N;
+int items = 0;
+int first = 0;
+int last = 0;
+
+
+
+void funexec(fatores a) {
+    printf("%c %d\n", a.a, a.b);
 }
 
-void agendarExecucao(void (*func)(pessoa), pessoa p) {
-    (*func)(p);
+void agendarExecucao(void (*func)(fatores), fatores p) {
+    
+    buff[last].funcao = func;
+    buff[last].parametro = p;
+
+    last++;
+    items++;
+    return items - 1;
 }
 
+void despachar() {
 
+}
 
 
 int main() {
 
-    pessoa x;
-    x.genero = 'm'; x.idade = 23;
+    fatores a;
+    a.a = 1; a.b = 5;
 
-    agendarExecucao(funexec, x);
+    agendarExecucao(funexec, a);
     
-
+    
 
 
 
