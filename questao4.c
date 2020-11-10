8c18fb10cc1973ce84b99bf994ed5d9a6b95bd49
Marcelo <mmv147.valois@gmail.com>
2020-11-10 01:35:32 -0300

    QUESTÃO PRONTA

--- questao4.c
+++ questao4.c
@@ -2,12 +2,14 @@
 #include <pthread.h> 
 #include <stdlib.h>
 
-#define BUFFER_SIZE 20
-#define N 5
+#define NUM_ITEMS 500 // QUANTIDADE DE REQUISIÇÕES QUE SERÃO GERADAS
+#define BUFFER_SIZE 20 // O TAMANHO DO BUFFER
+#define N 5  //O NÚMERO MÁXIMO DE THREADS
 
 typedef struct fatores {
     int a;
     int b;
+    int id;
 } fatores;
 
 typedef struct buffer {
@@ -22,20 +24,23 @@ int total = 0;
 int first = 0;
 int last = 0;
 
+int ready[NUM_ITEMS] = {0};
+int result[NUM_ITEMS];
+
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t mutex2 = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t empty = PTHREAD_COND_INITIALIZER;
 pthread_cond_t fill = PTHREAD_COND_INITIALIZER;
+pthread_cond_t unavailable = PTHREAD_COND_INITIALIZER;
+pthread_cond_t resultado = PTHREAD_COND_INITIALIZER;
 
 
 
 int agendarExecucao(void* (*func)(void*), fatores p) {
 
-    printf("EN ");
-
     pthread_mutex_lock(&mutex);
 
-    while(items == BUFFER_SIZE) pthread_cond_wait(&fill, &mutex);
+    while(items == BUFFER_SIZE) pthread_cond_wait(&fill, &mutex); //CASO O BUFFER ESTEJA CHEIO
 
     buffer aux;
     aux.funcao = func; aux.parametro = p;
@@ -45,7 +50,7 @@ int agendarExecucao(void* (*func)(void*), fatores p) {
 
     if(last == BUFFER_SIZE) last = 0;
         
-    if (items == 1) pthread_cond_broadcast(&empty);
+    if (items == 1) pthread_cond_broadcast(&empty); //VARIÁVEL DE CONDIÇÃO
 
     pthread_mutex_unlock(&mutex);
     
@@ -53,23 +58,25 @@ int agendarExecucao(void* (*func)(void*), fatores p) {
 }
 
 
-void* funexec(void* params) {
+void* funexec(void* params) {  //FUNÇÃO GENÉRICA PEDIDA PELA QUESTÃO. ESSA RETORNA A SOMA DOS DOIS VALORES
 
-    printf("Thread criada\n");
+    fatores dados = *((fatores *)params);
 
-    fatores a = *((fatores *)params);
+    ready[dados.id] = 1;
 
-    printf("%d %d\n", a.a, a.b);
+    result[dados.id] = dados.a + dados.b;
 
-    int i = 0;
-    for (i=0; i<1000000;i++){
-        i+i;
-    }
+    printf("Dados recebidos na thread com id %d: %d %d\n", dados.id, dados.a, dados.b);
 
     free(params);
 
     pthread_mutex_lock(&mutex2);
+    
     available++;
+
+    pthread_cond_signal(&unavailable);
+    pthread_cond_signal(&resultado);
+    
     pthread_mutex_unlock(&mutex2);
 
     pthread_exit(NULL);
@@ -77,43 +84,37 @@ void* funexec(void* params) {
 
 
 
-void *despachante() {
+void *despachante() { // ESSA É A THREAD DESPACHANTE
 
     void* (*func)(void*);
     int i;
 
-    printf("COMEÇOU DESPACHANTE\n");
-
-    for(i = 0; i<500; i++) {
+    for(i = 0; i < NUM_ITEMS; i++) {
         
         pthread_mutex_lock(&mutex);
 
         available--;
 
-        while (items == 0) pthread_cond_wait(&empty, &mutex);
+        while (items == 0) pthread_cond_wait(&empty, &mutex); //ESPERA ALGUM ITEM NO BUFFER
 
-        while (available == 0) {
-        ;
-        }
+        while (available == 0) pthread_cond_wait(&unavailable, &mutex); //ESPERA ALGUMA THREAD DISPONÍVEL
 
         pthread_t consumer_thread;
-        fatores* params = (fatores*) malloc(sizeof(fatores));
+        fatores* params = (fatores*) malloc(sizeof(fatores));  
         buffer aux = buff[first];
 
         func = aux.funcao; 
         params->a = aux.parametro.a;
         params->b = aux.parametro.b;
+        params->id = i;  // ID DA THREAD
 
-
-        printf("first: %d     %d %d\n", first, params->a, params->b);
-
-        pthread_create(&consumer_thread, NULL, func, (void*) params);
+        pthread_create(&consumer_thread, NULL, func, (void*) params);  //CRIA AS THREADS QUE EXECUTAM FUNEXEC
 
         items--; first++;
 
         if(first == BUFFER_SIZE) first = 0;
 
-        pthread_cond_signal(&fill);
+        if(items == BUFFER_SIZE - 1) pthread_cond_signal(&fill);
 
         pthread_mutex_unlock(&mutex);
 
@@ -121,25 +122,36 @@ void *despachante() {
 
     pthread_exit(NULL);
 
+}
 
+void pegarResultadoExecucao(int chave) {
+
+    pthread_mutex_lock(&mutex2);
+
+    while (ready[chave] == 0) pthread_cond_wait(&resultado, &mutex2); //ESPERA A THREAD ENTREGAR O RESULTADO
+
+    if (ready[chave])
+        printf("O resultado para a chave %d eh %d\n", chave, result[chave]);
+
+    pthread_mutex_unlock(&mutex2);
 }
 
 
 int main() {
 
     pthread_t despachante_thread;
-    int i, teste;
+    int i, chave;
 
     fatores factors;
     
     pthread_create(&despachante_thread, NULL, despachante, NULL);
 
-    for (i = 0; i < 500; i++) {
-        factors.a = i; factors.b = i;
-        agendarExecucao(funexec, factors);
-    }
+    for (i = 0; i < NUM_ITEMS; i++) {
+        factors.a = rand() % 1000; factors.b = rand() % 1000;  //PEGA NÚMEROS ALEATÓRIOS DE 0 A 1000
+        chave = agendarExecucao(funexec, factors);  //AGENDA EXECUÇÃO
 
-    
+        pegarResultadoExecucao(chave);
+    }
 
     pthread_exit(NULL);
 
